58a2e4e12a571eeb4a68d9ec9832f7f84cd7b799
hjpark625 <zero950@naver.com>
2022-10-03 14:22:48 +0900

    design: footer 완성 및 적용

---
+++ src/components/Footer.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Footer = () => {
+  return (
+    <CopyRightFooter>
+      &copy; 2022. Heeju Park all rights reserved.
+    </CopyRightFooter>
+  );
+};
+
+export default Footer;
+
+const CopyRightFooter = styled.footer`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100px;
+  font-size: 30px;
+  border-top: 2px solid lightgray;
+  background-color: inherit;
+  margin-top: 10rem;
+`;
